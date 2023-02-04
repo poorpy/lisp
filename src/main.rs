a3754d319e6db722327b573bf4e4b44e9f996a99
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2023-02-04 20:29:47 +0100

    feat: better pest grammar

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
 mod core;
 mod parser;
 
-fn main() {}
+use pest::Parser;
+
+fn main() {
+    let input = std::fs::read_to_string("input.lisp").expect("cannot read file");
+    let pairs =
+        parser::LispParser::parse(parser::Rule::lisp, &input).unwrap_or_else(|e| panic!("{}", e));
+
+    for pair in pairs {
+        // A pair is a combination of the rule which matched and a span of input
+        println!("Rule:    {:?}", pair.as_rule());
+        println!("Span:    {:?}", pair.as_span());
+        println!("Text:    {}", pair.as_str());
+
+        for inner in pair.into_inner() {
+            println!("Rule:    {:?}", inner.as_rule());
+            println!("Span:    {:?}", inner.as_span());
+            println!("Text:    {}", inner.as_str());
+        }
+    }
+}
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,27 +1,6 @@
-use std::collections::HashSet;
+#[cfg(debug_assertions)]
+const _GRAMMAR: &str = include_str!("grammar.pest");
 
-use nom::error::{ErrorKind, ParseError};
-use thiserror::Error;
-
-mod atom;
-
-#[derive(Debug, Error, PartialEq, Clone)]
-pub enum ParserError<I> {
-    #[error("symbol {symbol} contains illegal characters: {illegal:?}")]
-    SymbolContainsInvalid {
-        symbol: String,
-        illegal: HashSet<char>,
-    },
-    #[error("internal parser failed with error {1:?} for input {0:?}")]
-    Nom(I, ErrorKind),
-}
-
-impl<I> ParseError<I> for ParserError<I> {
-    fn from_error_kind(input: I, kind: ErrorKind) -> Self {
-        ParserError::Nom(input, kind)
-    }
-
-    fn append(_: I, _: ErrorKind, other: Self) -> Self {
-        other
-    }
-}
+#[derive(Parser)]
+#[grammar = "parser/grammar.pest"]
+pub struct LispParser;
