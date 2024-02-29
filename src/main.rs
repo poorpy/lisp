6ee5e27eb053930fa3abb4083ef7e799cd30d715
bartosz marczyński <marczynski.bartosz@gmail.com>
2024-02-29 19:55:58 +0100

    feat: add repl

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,49 @@ use env::Env;
 use eval::Expr;
 use pest::Parser;
 
-fn main() {
-    let input = std::fs::read_to_string("input.lisp").expect("cannot read file");
-    let pairs =
-        parser::LispParser::parse(parser::Rule::lisp, &input).unwrap_or_else(|e| panic!("{}", e));
+use rustyline::error::ReadlineError;
+use rustyline::DefaultEditor;
 
+fn main() -> anyhow::Result<()> {
+    let mut rl = DefaultEditor::new()?;
     let mut env = Env::default();
+
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                if let Err(err) = rl.add_history_entry(line.as_str()) {
+                    println!("failed to add history entry: {err}")
+                }
+                if let Err(err) = handle(line, &mut env) {
+                    println!("failed to handle line: {err:?}")
+                }
+            }
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("readline error: {:?}", err);
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn handle(input: String, env: &mut Env) -> anyhow::Result<()> {
+    let pairs = parser::LispParser::parse(parser::Rule::lisp, &input)?;
 
     for pair in pairs {
         match parser::read(pair) {
-            Ok(ast) => match eval::eval(Expr::from(ast), &mut env) {
+            Ok(ast) => match eval::eval(Expr::from(ast), env) {
                 Ok(expr) => println!("{expr}"),
                 Err(e) => println!("{e:?}"),
             },
             Err(e) => println!("{e:?}"),
         }
     }
+
+    Ok(())
 }
